13b94ee9ea468a0003bf0d754e8ba9cfd22bff5f
michael <michaelosnowden@gmail.com>
2014-07-27 15:10:02 -0700

    Initial Commit

---
+++ StackOverflow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StackOverflow
+//
+//  Created by Michael Snowden on 7/27/14.
+//  Copyright (c) 2014 MichaelSnowden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StackOverflow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StackOverflow
+//
+//  Created by Michael Snowden on 7/27/14.
+//  Copyright (c) 2014 MichaelSnowden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
